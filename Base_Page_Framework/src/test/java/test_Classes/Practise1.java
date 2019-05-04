9dd9d6502e94e22fc1720827a0bdc5cd38f406e2
Sony <Sony@Sony-PC>
2019-05-05 01:52:54 +0530

    third commit

--- Base_Page_Framework/src/test/java/test_Classes/Practise1.java
+++ Base_Page_Framework/src/test/java/test_Classes/Practise1.java
@@ -13,7 +13,7 @@ public class Practise1 {
 	@Test()
 	public void testA(){
 		System.out.println("In testB of priority 1");
-		assertEquals("Rathore", "Akash");
+		assertEquals("Rathores", "Akash");
 	}
 	/*@Test(priority=1,groups={"smoke"})
 	public void testC(){
