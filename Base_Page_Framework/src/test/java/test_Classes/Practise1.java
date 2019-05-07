c443b0b4b82a7582c70d4d5fb12189d9f351c5e2
Sony <Sony@Sony-PC>
2019-05-07 23:44:21 +0530

    gvxv

--- Base_Page_Framework/src/test/java/test_Classes/Practise1.java
+++ Base_Page_Framework/src/test/java/test_Classes/Practise1.java
@@ -13,7 +13,7 @@ public class Practise1 {
 	@Test()
 	public void testA(){
 		System.out.println("In testB of priority 1");
-		assertEquals("dfghfhARathojhresfgh", "Akash");
+		assertEquals("dfghfsdhARathojhresfgh", "Akash");
 	}
 	/*@Test(priority=1,groups={"smoke"})
 	public void testC(){
