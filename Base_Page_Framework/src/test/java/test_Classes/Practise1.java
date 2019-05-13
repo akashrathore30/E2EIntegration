77031c51ce0c783b965f2a65f4eebb097f5eef2f
Sony <Sony@Sony-PC>
2019-05-14 01:11:30 +0530

    fdff

--- Base_Page_Framework/src/test/java/test_Classes/Practise1.java
+++ Base_Page_Framework/src/test/java/test_Classes/Practise1.java
@@ -13,7 +13,7 @@ public class Practise1 {
 	@Test()
 	public void testA(){
 		System.out.println("In testB of priority 1");
-		assertEquals("Akasdsdhs", "Akashjh");
+		assertEquals("Akasddfdsdhs", "Akashjh");
 	}
 	/*@Test(priority=1,groups={"smoke"})
 	public void testC(){
